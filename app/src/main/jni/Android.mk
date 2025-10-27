d8719d66a910fd28029944671c2416191c9e057d
VCX <88580298+Viablecobra@users.noreply.github.com>
2025-10-27 10:00:27 +0530

    Add linker flags for shared library build

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,4 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := mc
 LOCAL_SRC_FILES := mc-init.cpp
+LOCAL_LDLIBS    := -L$(LOCAL_PATH)/$(TARGET_ARCH_ABI) -llog -ldl
+TARGET_NO_UNDEFINED_LDFLAGS :=
 include $(BUILD_SHARED_LIBRARY)
